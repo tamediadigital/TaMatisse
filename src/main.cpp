66bef0eea531a9dd042772d77f200b9de5f83a28
j0b1 <jonnybie@gmail.com>
2017-11-08 00:17:37 +0100

    added fibonacci spiral function

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,29 @@
 // Simple example for driving the TaMatisse, our little painting bot around
 TaMatisse werdBot;
 
+long f(long n = 0);
+
 void setup() {
 }
 
 void loop() {
+  //paint8();
+  f();
+}
+
+long f(long n = 0) {
+  int radius = n;
+  int angle = 90;
+  werdBot.bogeNachLinksMoole(angle, radius);
+  return f(n+n+1);
+}
+
+void box() {
+  werdBot.graduusMoole(40);
+  werdBot.ufEmPunktNachLinksDreie(90);
+}
+
+void paint8() {
   // draw an 8 on the floor. Run for cover. :)
   for (int i=0; i<4; i++) {
     // turn left
