a74819f5f33128a65988f0f305afd95bc296d526
nfinke <nicolai@tutti.ch>
2017-11-09 10:46:13 +0100

    Fixed (?) issue with Prototypes header

--- src/Prototypes.h
+++ src/Prototypes.h
@@ -5,7 +5,8 @@
 */
 
 /* functions to draw a fibonacci spiral */
-long f(long f1 = 0, long f2 = 1)
+long f(long f1 = 0, long f2 = 1);
+
 double squareGrowth(double n);
 
 /* draw a box with mm size */
